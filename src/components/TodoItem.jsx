7facfa9b6c75ee05688dc11e025d126662a0ac95
윤남경 <nk.yun@bpmg.biz>
2024-04-16 23:25:48 +0900

    todo list init

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,27 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onDeleteItem = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        readOnly
+        checked={isDone}
+        onClick={onChangeCheckbox}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onDeleteItem}>삭제</button>
+    </div>
+  );
+};
+
+export default TodoItem;
